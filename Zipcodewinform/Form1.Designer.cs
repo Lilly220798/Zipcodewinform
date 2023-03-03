330183444f6e5162a4574e8f42a66c9648bcdf3e
Lilly220798 <tulanilinca@gmail.com>
2023-03-03 09:59:16 +0200

    Add project files.

---
+++ Zipcodewinform/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Zipcodewinform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtZipCode = new System.Windows.Forms.TextBox();
+            this.txtShoppingValue = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(217, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(385, 54);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SuperMaket App";
+            // 
+            // txtZipCode
+            // 
+            this.txtZipCode.Location = new System.Drawing.Point(75, 178);
+            this.txtZipCode.Multiline = true;
+            this.txtZipCode.Name = "txtZipCode";
+            this.txtZipCode.Size = new System.Drawing.Size(242, 34);
+            this.txtZipCode.TabIndex = 1;
+            // 
+            // txtShoppingValue
+            // 
+            this.txtShoppingValue.Location = new System.Drawing.Point(482, 178);
+            this.txtShoppingValue.Multiline = true;
+            this.txtShoppingValue.Name = "txtShoppingValue";
+            this.txtShoppingValue.Size = new System.Drawing.Size(246, 34);
+            this.txtShoppingValue.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(70, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Zip Code";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(477, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(198, 29);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Shopping Value";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(110, 281);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(124, 46);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(333, 281);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(125, 46);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(534, 281);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(125, 46);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtShoppingValue);
+            this.Controls.Add(this.txtZipCode);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtZipCode;
+        private System.Windows.Forms.TextBox txtShoppingValue;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Zipcodewinform/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.AccessControl;
+using System.Reflection.Emit;
+
+namespace Zipcodewinform
+{
+    public partial class Form1 : Form
+    {
+        private string pattern = @"^\d{5}(?:[-\s]\d{4})?$";
+        private Dictionary<string, decimal> customerShoppingValues = new Dictionary<string, decimal>();
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string zipCode = txtZipCode.Text;
+            decimal shoppingValue = decimal.Parse(txtShoppingValue.Text);
+
+            bool isValid = Regex.IsMatch(zipCode, pattern);
+            if (isValid)
+            {
+                if (customerShoppingValues.ContainsKey(zipCode))
+                {
+                    customerShoppingValues[zipCode] += shoppingValue;
+                }
+                else
+                {
+                    customerShoppingValues.Add(zipCode, shoppingValue);
+                }
+
+                MessageBox.Show("Shopping value added to customer.");
+            }
+            else
+            {
+                MessageBox.Show($"{zipCode} is not a valid zip code. Shopping value not added to customer.");
+            }
+
+            txtZipCode.Clear();
+            txtShoppingValue.Clear();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string fileName = "supermarket.txt";
+           
+            string filePath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\", fileName));
+            
+
+            MessageBox.Show($"{filePath}");
+
+            using (StreamWriter writer = new StreamWriter(filePath, true))
+            {
+                foreach (KeyValuePair<string, decimal> customer in customerShoppingValues)
+                {
+                    string customerData = $"{DateTime.Now.ToString()}, {customer.Value:C}, {customer.Key}";
+
+                    writer.WriteLine(customerData);
+                }
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
